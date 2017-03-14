894f3666fb546b17b9de1e9a673332cfe50bef86
Charlie-L-Zhang <Charlie.Liao.xyz@gmail.com>
2017-03-14 20:11:58 +0000

    Google Drive API test

---
+++ NetCore_GoogleDriveApiTest/Program.cs
@@ -0,0 +1,122 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DriveQuickstart
+{
+    class Program
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/drive-dotnet-quickstart.json
+        static string[] Scopes = { DriveService.Scope.DriveReadonly };
+        static string ApplicationName = "Drive API .NET Quickstart";
+
+        static MemoryStream DownloadFile(DriveService driveService, string fileId)
+        {
+            var request = driveService.Files.Get(fileId);
+            var stream = new System.IO.MemoryStream();
+
+            //// Add a handler which will be notified on progress changes.
+            //// It will notify on each chunk download and when the
+            //// download is completed or failed.
+            //request.MediaDownloader.ProgressChanged +=
+            //    (IDownloadProgress progress) =>
+            //    {
+            //        switch (progress.Status)
+            //        {
+            //            case DownloadStatus.Downloading:
+            //                {
+            //                    Console.WriteLine(progress.BytesDownloaded);
+            //                    break;
+            //                }
+            //            case DownloadStatus.Completed:
+            //                {
+            //                    Console.WriteLine("Download complete.");
+            //                    break;
+            //                }
+            //            case DownloadStatus.Failed:
+            //                {
+            //                    Console.WriteLine("Download failed.");
+            //                    break;
+            //                }
+            //        }
+            //    };
+            request.Download(stream);
+
+            return stream;
+        }
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = @"c:\users\derenl\Documents\"; //System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 10;
+            listRequest.Fields = "nextPageToken, files(id, name)";
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
+                .Files;
+            Console.WriteLine("Files:");
+            if (files != null && files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    Console.WriteLine("{0} ({1})", file.Name, file.Id);
+                    if (file.Name.StartsWith("client_secret"))
+                    {
+                        using (var stream = DownloadFile(service, file.Id))
+                        {
+                            stream.Seek(0, SeekOrigin.Begin);
+                            using (var sr = new StreamReader(stream))
+                            {
+                                Console.WriteLine(sr.ReadToEnd());
+                            }
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No files found.");
+            }
+
+            Console.Read();
+        }
+
+    }
+}
