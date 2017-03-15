4a8eb296bfbe03b5ff61e4c25636f4f547c4b8af
Charlie-L-Zhang <Charlie.Liao.xyz@gmail.com>
2017-03-15 19:28:25 +0000

    Google Drive Web Application Authentication flow
    ASP.NET MVC .net framework 4.5

---
+++ NetFrameSiteOnDrive/Controllers/AuthCallbackController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetFrameSiteOnDrive.Controllers
+{
+    public class AuthCallbackController : Google.Apis.Auth.OAuth2.Mvc.Controllers.AuthCallbackController
+    {
+        //// GET: AuthCallback
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        protected override Google.Apis.Auth.OAuth2.Mvc.FlowMetadata FlowData
+        {
+            get { return new AppFlowMetadata(); }
+        }
+    }
+}
---
+++ NetFrameSiteOnDrive/Controllers/GoogleDriveController.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+using Google.Apis.Auth.OAuth2.Mvc;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+
+namespace NetFrameSiteOnDrive.Controllers
+{
+    public class GoogleDriveController : AsyncController
+    {
+        //public async Task<ActionResult> IndexAsync(CancellationToken cancellationToken)
+        //{
+        //    return Content("Async controller");
+        //}
+
+        //public ActionResult Index()
+        //{
+        //    return Content("something");
+        //}
+
+        public async Task<ActionResult> IndexAsync(CancellationToken cancellationToken)
+        {
+            var result = await new AuthorizationCodeMvcApp(this, new AppFlowMetadata()).
+                AuthorizeAsync(cancellationToken);
+
+            if (result.Credential != null)
+            {
+                var service = new DriveService(new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = result.Credential,
+                    ApplicationName = "ASP.NET MVC Sample"
+                });
+
+                // YOUR CODE SHOULD BE HERE..
+                // SAMPLE CODE:
+                var list = await service.Files.List().ExecuteAsync();
+                return Content("FILE COUNT IS: " + list.Files.Count());
+            }
+            else
+            {
+                return new RedirectResult(result.RedirectUri);
+            }
+        }
+    }
+}
