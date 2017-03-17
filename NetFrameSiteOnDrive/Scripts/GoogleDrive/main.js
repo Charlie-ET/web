5c0c896aef8f3543be027cd6ab171c9a86245eb9
Deren Liao <derenl@google.com>
2017-03-17 12:54:42 -0700

    Add password edit

--- NetFrameSiteOnDrive/Scripts/GoogleDrive/main.js
+++ NetFrameSiteOnDrive/Scripts/GoogleDrive/main.js
@@ -1,4 +1,27 @@
-﻿var myApp = angular.module('myApp', []);
+﻿class Pass {
+    constructor(id, user, pass, desc) {
+        this.id = id;
+        this.user = user;
+        this.pass = pass;
+        this.desc = desc;
+        this.passInType = "password";
+    }
+}
+
+function pass() {
+    if (arguments.length === 1) {
+        // assume input type is password json element
+        var x = arguments[0]
+        return new Pass(x.id, x.user, x.pass, x.desc);
+    } else if (arguments.length === 4) {
+        return new Pass(arguments[0], arguments[1], arguments[2], arguments[3]);
+    }
+    else {
+        throw "Unexpected number of input parameters";
+    }
+}
+
+var myApp = angular.module('myApp', []);
 myApp.controller('mainCtrl', ['$scope', '$http', function ($scope, $http) {
 
     // Set the default value of inputType
@@ -17,15 +40,21 @@ myApp.controller('mainCtrl', ['$scope', '$http', function ($scope, $http) {
 
     $scope.okPassword = function (){
         $scope.waitPassword = false;
+        $scope.viewCollection = [
+            //pass("i1", "name1", "p1", "d1")
+        ];
         // $scope.tableContent = "Now you see some content"
         $http.get("http://localhost:49906/GoogleDrive/table").then(
                 function (successResponse) {
                     $scope.tableContent = successResponse.data;
-                    $scope.tableJson = angular.fromJson($scope.tableContent)
-                    $scope.firstuser = $scope.tableJson.records[0].user
+                    $scope.tableJson = angular.fromJson($scope.tableContent);
+                    angular.forEach($scope.tableJson.records, function(value, key) {
+                        $scope.viewCollection.push(pass(value));
+                    });
                 },
                 function (errorResponse) {
                     // handle errors here
                 });
     }
+
 }]);
