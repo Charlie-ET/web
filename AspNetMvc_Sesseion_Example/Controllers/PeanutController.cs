bdece65755a19b062b1cd195f26eb1374829172d
Deren-Liao <derenl@google.com>
2017-04-01 23:48:05 +0100

    MVC session example.

---
+++ AspNetMvc_Sesseion_Example/Controllers/PeanutController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Template_AspNetMvc.Controllers
+{
+    public class PeanutController : Controller
+    {
+        // GET: Peanut
+        public ActionResult Index()
+        {
+            Session["Token"] = Guid.NewGuid().ToString();
+            return View();
+        }
+
+        // GET: /Peanut/Log/1
+        [HttpGet]
+        public ActionResult LogDefault()
+        {
+            return Content("Yes, Log()");
+        }
+
+        // GET: /Peanut/Log/1
+        [HttpGet]
+        public ActionResult Log(int? count)
+        {
+            return Content($"{count.ToString()}  {Session["Token"]}");
+        }
+    }
+}
